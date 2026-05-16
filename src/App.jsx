57f4c653a75842e7f818d1ad854308b5526f0da4
Carlostala04 <carlostala.dev@gmail.com>
2026-05-16 13:46:56 -0600

    cambios menores en el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
+import "./App.css";
+import Home from "./pages/Home";
 function App() {
   
-
   return (
     <>
-    <Home/>
+      <Home/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
