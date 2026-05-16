fe53a48e1e03c76dd49d8ed6afd324d08de78411
Carlostala04 <carlostala.dev@gmail.com>
2026-05-16 14:12:58 -0600

    se agrego el componente routes para poder manejar las rutas en la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+
 function App() {
-  
   return (
-    <>
-      <Home/>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
