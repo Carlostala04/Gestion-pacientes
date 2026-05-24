be98458adff2a92562a031014d6979b78e76b1a4
Carlostala04 <carlostala.dev@gmail.com>
2026-05-24 11:55:38 -0600

    se agregaron las rutas de la pantalla de login y recuperar contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ import PatientDeatils from './pages/PatientDeatils'
 import RegisterConsulta from './pages/RegisterConsulta'
 import EditPatient from './pages/EditPatient'
 import EditConsulta from './pages/EditConsulta'
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/Record" element={<Record/>}/>
       <Route path="/patient/:id" element={<PatientDeatils/>}/>
